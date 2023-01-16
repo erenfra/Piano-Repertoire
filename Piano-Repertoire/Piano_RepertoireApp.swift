2f93f04b225b27d3aad7a4eb2190f75359e324f2
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-01-16 12:09:41 -0600

    Initial Commit

---
+++ Piano-Repertoire/Piano_RepertoireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Piano_RepertoireApp.swift
+//  Piano-Repertoire
+//
+//  Created by Renato Oliveira Fraga on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Piano_RepertoireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
